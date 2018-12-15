011be881dde15eac9dfd1d2c18305a2e618059d3
i4cindia <36263802+i4cindia@users.noreply.github.com>
2018-12-15 19:07:33 +0530

    Adding test mobile PWA
    Adding test mobile PWA

---
+++ mobtest/main.js
@@ -0,0 +1,53 @@
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
+
+Notification.requestPermission(function(status) {
+    console.log('Notification permission status:', status);
+});
+
+function setNotifications() {
+	document.getElementById("divInfo").innerHTML=localStorage.getItem("NotificationRecord");
+}
+function clearNotifications() {
+	localStorage.setItem("NotificationRecord","");
+	setNotifications()
+}
+
+function callNotifier() {
+	console.log("notifier called");
+	message=document.getElementById("txtMessage").value;
+	if(message=="") message='Hello world!';
+	duration=document.getElementById("txtDuration").value;
+	console.log(duration);
+	dur=10;
+	if(!(isNaN(duration)||duration=="")){dur=duration;}
+	console.log(dur);
+	setTimeout(function(){ displayNotification(message); },dur*1000);
+}
+
+function displayNotification(message) {
+  if (Notification.permission == 'granted') {
+    navigator.serviceWorker.getRegistration().then(function(reg) {
+      reg.showNotification(message);
+    });
+  }
+  dt=new Date();
+  timeNow=dt.getTime();
+  localStorage.setItem("NotificationRecord", localStorage.getItem("NotificationRecord")+"<br/>"+"Notification on "+timeNow+" is "+message);
+  document.getElementById("divInfo").innerHTML=localStorage.getItem("NotificationRecord");
+}
